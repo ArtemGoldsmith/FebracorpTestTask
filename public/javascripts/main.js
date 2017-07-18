9aec3c5db114142b69395818b1607aa60f65d37f
Artem Saribekyan <asara4671@gmail.com>
2017-07-18 08:12:07 +0300

    # other try

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,54 @@
+"use strict";
+/**
+ * ------------------------------------------------------------------------------------------------------
+ * Variables
+ * ------------------------------------------------------------------------------------------------------
+ **/
 var itemsQuantity = 0;
 var itemsType;
 
-$('.options-button').on('click', function() {
-  $('.dropdown-block').toggleClass('visible');
+/**
+ * ------------------------------------------------------------------------------------------------------
+ * Custom Functions
+ * ------------------------------------------------------------------------------------------------------
+ **/
+
+$.fn.extend({
+  donetyping: function(callback, timeout) {
+    timeout = timeout || 1e3; // 1 second
+    var timeoutReference,
+    doneTyping = function(el) {
+      if ( !timeoutReference ) return;
+      timeoutReference = null;
+      callback.call(el);
+    };
+    return this.each(function(i, el) {
+      var $el = $(el);
+      $el.is(':input') && $el.on('keyup keypress paste', function(e) {
+        if ( e.type === 'keyup' && e.keyCode !== 8 ) return;
+        if ( timeoutReference ) clearTimeout(timeoutReference);
+        timeoutReference = setTimeout(function() {
+          doneTyping(el);
+        }, timeout);
+      }).on('blur', function() {
+        doneTyping(el);
+      });
+    });
+  }
 });
+
+/**
+ * ------------------------------------------------------------------------------------------------------
+ * Main Functions
+ * ------------------------------------------------------------------------------------------------------
+ **/
 
 function changeItemsType(type) {
   itemsType = type;
   $('#quantity').removeAttr('disabled').css('border', '1px solid #ff0000');
   $('.main-block .item-wrapper').html('');
   if ( itemsQuantity > 0 ) {
-    for ( i = 0; i < itemsQuantity; i++ ) {
+    for ( var i = 0; i < itemsQuantity; i++ ) {
       if ( i < 3 ) {
         $('.main-block .item-wrapper').each(function() {
           $(this).append('<div class="item item-a">Item ' + itemsType + [i + 1] + '</div>');
@@ -18,13 +56,7 @@ function changeItemsType(type) {
       }
     }
   }
-}
-
-$('#quantity').on('input', function() {
-  itemsQuantity = $(this).val();
-  changeItemsType(itemsType);
-  sendItemsQuantity();
-});
+};
 
 function sendItemsQuantity() {
 
@@ -43,9 +75,25 @@ function sendItemsQuantity() {
   });
 }
 
+/**
+ * ------------------------------------------------------------------------------------------------------
+ * Events
+ * ------------------------------------------------------------------------------------------------------
+ **/
+
 $(document).mouseup(function(e) {
   var el = $('.dropdown-block');
   if ( !el.is(e.target) && el.has(e.target).length === 0 && el.hasClass('visible') ) {
     el.removeClass('visible');
   }
+});
+
+$('.options-button').on('click', function() {
+  $('.dropdown-block').toggleClass('visible');
+});
+
+$('#quantity').donetyping(function(){
+  itemsQuantity = $(this).val();
+  changeItemsType(itemsType);
+  sendItemsQuantity();
 });
