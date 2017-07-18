08a3b8cb671d0f007c562abe6b0ed0320430739d
Artem Saribekyan <asara4671@gmail.com>
2017-07-18 07:50:50 +0300

    # other try

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,12 +9,13 @@ function changeItemsType(type) {
   itemsType = type;
   $('#quantity').removeAttr('disabled').css('border', '1px solid #ff0000');
   $('.main-block .item-wrapper').html('');
-  console.log(itemsType);
   if ( itemsQuantity > 0 ) {
-    for ( i = 0; i < 3; i++ ) {
-      $('.main-block .item-wrapper').each(function() {
-        $(this).append('<div class="item item-a">Item ' + itemsType + [i+1] + '</div>');
-      });
+    for ( i = 0; i < itemsQuantity; i++ ) {
+      if ( i < 3 ) {
+        $('.main-block .item-wrapper').each(function() {
+          $(this).append('<div class="item item-a">Item ' + itemsType + [i + 1] + '</div>');
+        });
+      }
     }
   }
 }
